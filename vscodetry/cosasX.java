9d41f1159308521606e79bf7bb5bc8f66e086e2b
Simon Garcia <157841549+SimonGarcia01@users.noreply.github.com>
2024-03-08 09:01:28 -0500

    added the exam 1 in lab 6

--- vscodetry/cosasX.java
+++ vscodetry/cosasX.java
@@ -1,19 +1,34 @@
+import java.util.Scanner;
+
 public class cosasX {
     public static void main(String[] args){
-        int[] array = {1,2,3,4,5,6};
-        System.out.print(printArray(array));
+        Scanner sk = new Scanner(System.in);
+        int vote = 0;
+        boolean checkSk = false;
+        
+        do{
+
+            vote = menu(sk);
+
+            if(!(vote==1||vote==2||vote==3||vote==4||vote==5||vote==6)){
+                System.out.print("El valor debe ser del 1 - 6. Intente de nuevo.");
+                checkSk = true;
+            } else{
+                checkSk = false;
+            }
+
+
+        }while(checkSk);
+
+        System.out.print(vote);
     }
     
-    public static String printArray(int[] printingArray) {
+    public static int menu(Scanner sk) {
+        System.out.print("\nIngrese su voto:\n\t1. El chico y la garza\n\t2. Elemental");
+        System.out.print("\n\t3. Robot Dreams\n\t5. Spider-Man: Cruzando el Multiverso");
+        System.out.println("\nSi desea salir la ultima opcion:\n\t6. Terminar las votaciones");
+        System.out.print("Su voto: ");
 
-        String printedArray = "";
-
-        for(int n = printingArray.length-1; n >= 0; n--) {
-            printedArray = printingArray[n] + printedArray;
-
-        }
-
-        return printedArray;
-
+        return sk.nextInt();
     }
 }
